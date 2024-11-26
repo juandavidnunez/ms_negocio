0ca28893d760b53323ad2205f69fea1e3ae17032
juandavidnunez <juandavidnunez83@gmail.com>
2024-11-26 06:17:08 -0500

    juan

--- database/migrations/1732409232304_categoria_productos.ts
+++ database/migrations/1732409232304_categoria_productos.ts
@@ -1,7 +1,7 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'categorias_productos'
+  protected tableName = 'categoria_productos'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
