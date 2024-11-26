f579058d8b5d61008d8068a51c3faaa5971016bd
juandavidnunez <juandavidnunez83@gmail.com>
2024-11-26 04:03:28 -0500

    juan

--- database/migrations/1730574257509_direccions.ts
+++ database/migrations/1730574257509_direccions.ts
@@ -1,7 +1,7 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'direcciones'
+  protected tableName = 'direccions'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
